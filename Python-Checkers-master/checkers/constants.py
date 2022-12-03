4c507924ebc447bc5106744a5214142a2ac6f295
Захар Ларионов <zaxgl2004@gmail.com>
2022-12-03 18:47:57 +0300

    first commit

---
+++ Python-Checkers-master/checkers/constants.py
@@ -0,0 +1,15 @@
+import pygame
+
+WIDTH, HEIGHT = 600, 600
+ROWS, COLS = 8, 8
+SQUARE_SIZE = WIDTH//COLS
+
+# rgb
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+GREY = (128,128,128)
+
+CROWN = pygame.transform.scale(pygame.image.load('C:/Users/zaxgl/Desktop/asa/crown.png'), (44, 25))
+JABA = pygame.transform.scale(pygame.image.load('D:/браузер/jaba.png'), (100, 100))
---
+++ Python-Checkers-master/checkers/menu.py
@@ -0,0 +1,18 @@
+import pygame
+import pygame_menu
+
+
+def set_difficulty(value, difficulty):
+    # Do the job here !
+    pass
+
+def start_the_game():
+    # Do the job here !
+    pass
+
+menu = pygame_menu.Menu(300, 400, 'Welcome', theme=pygame_menu.themes.THEME_BLUE)
+
+menu.add_text_input('Name :', default='John Doe')
+menu.add_selector('Difficulty :', [('Hard', 1), ('Easy', 2)], onchange=set_difficulty)
+menu.add_button('Play', start_the_game)
+menu.add_button('Quit', pygame_menu.events.EXIT)
