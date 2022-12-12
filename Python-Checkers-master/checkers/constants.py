c359f2c9dad79aafe0be1643f8b52a0d592d5508
Kuznetsovve <114419300+Kuznetsovve@users.noreply.github.com>
2022-12-12 14:10:32 +0300

    Исправлено отображение меню

--- Python-Checkers-master/checkers/constants.py
+++ Python-Checkers-master/checkers/constants.py
@@ -18,5 +18,5 @@ LIGHT_GREEN = (128, 255, 128)
 # Изображения
 WOLF = pygame.transform.scale(pygame.image.load('Wolf.png'), (80, 80))
 SHEEP = pygame.transform.scale(pygame.image.load('Sheep.png'), (80, 80))
-menu_background = pygame.image.load('Logo_klovn.png')
+MENU_BACKGROUND = pygame.transform.scale(pygame.image.load('Logo_klovn.png'), (WIDTH, HEIGHT))
 pass
--- Python-Checkers-master/main.py
+++ Python-Checkers-master/main.py
@@ -1,17 +1,21 @@
-# Assets: https://techwithtim.net/wp-content/uploads/2020/09/assets.zip
 import pygame
-from checkers.constants import WIDTH, HEIGHT, SQUARE_SIZE, RED
+from checkers.constants import WIDTH, HEIGHT, SQUARE_SIZE, MENU_BACKGROUND
 from checkers.game import Game
+from sys import exit
 
+pygame.init()
 
-
-import pygame
 FPS = 60
 
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption('Checkers')
+# Отрисовка окна
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
+# Название игры
+pygame.display.set_caption('Волки и овца')
 
+# Музыка
+alpha_shtrih = pygame.mixer.music.load('alpha_shtrih.mp3')
+pygame.mixer.music.play(-1)
 
 def get_row_col_from_mouse(pos):
     x, y = pos
@@ -19,29 +23,60 @@ def get_row_col_from_mouse(pos):
     col = x // SQUARE_SIZE
     return row, col
 
-def main():
-    run = True
+def menu(start):
+    """Создание меню"""
+    pygame.init()
+
+    while start:
+
+        # Фон
+        pygame.display.update()
+        screen.blit(MENU_BACKGROUND, (0, 0))
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                start = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos1, pos2 = pygame.mouse.get_pos()
+
+                # Кнопка для старта
+                if (pos1 - WIDTH//2) ** 2 + (pos2 - HEIGHT//2) ** 2 <= (WIDTH * 0.096) ** 2:
+                    main(True)
+                    start = False
+
+    pygame.quit()
+    exit()
+
+def main(run):
+    """Цикл игры"""
+    pygame.init()
     clock = pygame.time.Clock()
-    game = Game(WIN)
+    game = Game(screen)
 
     while run:
         clock.tick(FPS)
 
-        if game.winner() != None:
+        # Завершение игры при определении победителя раунда
+        '''if game.winner() != None:
             print(game.winner())
-            run = False
+            run = False'''
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-            
+
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pos = pygame.mouse.get_pos()
                 row, col = get_row_col_from_mouse(pos)
                 game.select(row, col)
 
         game.update()
-    
-    pygame.quit()
 
-main()
+    pygame.quit()
+    exit()
+
+
+menu(True)
+
+main(True)
